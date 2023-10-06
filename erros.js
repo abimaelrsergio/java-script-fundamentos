7ad0519ce45538a79fd78a94e5f1402a9f6aba36
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-06 00:07:28 -0300

    Erros e stactrace

---
+++ erros.js
@@ -0,0 +1,11 @@
+// consts não podem ser criadas sem valor
+//const numero;
+const teste2 = 2;
+// falta o fechamento do parênteses
+//console.log(teste2;
+
+// a variável cpf não foi definida (criada) antes de ser usada.
+//console.log(cpf);
+
+
+
