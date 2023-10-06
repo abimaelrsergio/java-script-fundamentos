04d945ce37e374fbb53592657138f9ef3b19b3d1
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-06 11:00:25 -0300

    Operador ternário

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+/*if (idadeCliente >= idadeMinima){
+    console.log('Pode beber alcool');
+} else {
+    console.log('Não pode beber alcool');
+}*/
+
+// usando ternário:
+console.log(idadeCliente >= idadeMinima ? 'Pode beber alcool' : 'Não pode beber alcool');
