7b0d87c05bd78e16945dfbfdbf8149fdd589c65c
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-06 11:07:08 -0300

    Template literal

---
+++ template-strings.js
@@ -0,0 +1,6 @@
+const nome = 'Abimael';
+const idade = 46;
+const cidadeDeNascimento = 'São Paulo'
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci em ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
